821f47f1c4be645318536159c5d045a7b3edd37a
Shane <sfondeur@academic.rrc.ca>
2018-11-06 17:35:47 -0600

    Ran the migration for the Active Admin tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_193222) do
+ActiveRecord::Schema.define(version: 2018_11_06_233341) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.string "author_type"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -20,7 +46,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_193222) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
-    t.decimal "price", precision: 6, scale: 2
+    t.decimal "price", precision: 15, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "order_id"
@@ -30,7 +56,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_193222) do
   create_table "orders", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.decimal "subtotal", precision: 6, scale: 2
+    t.decimal "subtotal", precision: 15, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -46,7 +72,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_193222) do
 
   create_table "product_variants", force: :cascade do |t|
     t.string "title"
-    t.decimal "price", precision: 6, scale: 2
+    t.decimal "price", precision: 15, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_id"
@@ -56,7 +82,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_193222) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.decimal "price", precision: 6, scale: 2
+    t.decimal "price", precision: 15, scale: 2
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
