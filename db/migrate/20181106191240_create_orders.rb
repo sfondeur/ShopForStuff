4219d297c352a930cd0b565c1d04d2a7b7008e5c
Shane <sfondeur@academic.rrc.ca>
2018-11-06 13:47:10 -0600

    Created tables needed and ran a db:migrate to add models to the schema.

---
+++ db/migrate/20181106191240_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.string :first_name
+      t.string :last_name
+      t.decimal :subtotal, precision: 15, scale: 2
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20181106192830_add_product_to_product_variant.rb
@@ -0,0 +1,5 @@
+class AddProductToProductVariant < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :product_variants, :product, foreign_key: true
+  end
+end
