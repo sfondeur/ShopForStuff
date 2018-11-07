8546bf522f55b759c53b310aacf11f9b978bcc0f
Shane <sfondeur@academic.rrc.ca>
2018-11-06 18:15:08 -0600

    Created seeds file for the data populating

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Product.destroy_all
+Category.destroy_all
+ProductCategory.destroy_all
+
+Category.create(title: 'Electronics')
+Category.create(title: 'Furniture')
+Category.create(title: 'Miscellaneous')
+
+elec = Category.find_by(title: 'Electronics')
+furn = Category.find_by(title: 'Furniture')
+misc = Category.find_by(title: 'Miscellaneous')
+
+elec.products.create title:'Xbox One X', description: 'Plays your Xbox games in 4K and 60fps', price: 499.99
+elec.products.create title:'Xbox One S', description: 'Slightly better than the original', price: 399.99
+elec.products.create title:'Playstation 4', description: 'Original PS4', price: 399.99
+elec.products.create title:'Playstation 4 Pro', description: 'Plays PS games in 60 fps', price: 499.99
+elec.products.create title:'Nintendo Switch', description: 'Plays Nintendo games', price: 449.99
+
+furn.products.create title: 'Damacio Recliner', description: 'Leather reclining chair', price: 899.99
+furn.products.create title: 'Chamberley Sectional', description: 'Grey fabric Sectional', price: 1699.99
+furn.products.create title:'TV Stand', description: 'Tv stand for your media related items', price: 199.99
+
+puts "Number of Categories created: #{Category.count}"
+puts "Number of Products created: #{Product.count}"
+
 
 # faker commerce stuff
 # For unique values only
