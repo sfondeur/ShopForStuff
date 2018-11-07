4b493c4ec26f494575c2d38c58ff1513940dd15f
Shane <sfondeur@academic.rrc.ca>
2018-11-06 19:10:57 -0600

    Implemented Index and Show actions for Product Controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,7 +1,11 @@
 class ProductController < ApplicationController
+  # collection of all Products
   def index
+    @products = Product.order(:title)
   end
 
+  # Specific Product by ID
   def show
+    @product = Product.find(params[:id])
   end
 end
