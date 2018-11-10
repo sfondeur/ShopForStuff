d941d2dc2bd8c2a37c47e8c3c0f1f412ae9216a8
Shane <sfondeur@academic.rrc.ca>
2018-11-09 19:28:40 -0600

    Added search controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,9 +1,11 @@
 class ProductController < ApplicationController
   # collection of all Products
   def index
-    @products = Product.order(:title)
+    # @products = Product.order(:title)
 
     # Product.where("created_at >= ?", 1.week.ago.utc).order("DESC, created_at DESC")
+
+    @products = Product.order(:title).page(params[:page]).per(4)
   end
 
   # Specific Product by ID
