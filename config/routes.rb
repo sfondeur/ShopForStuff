0586d5b3db00e33f0440d74f9f7e6e28708f7540
Shane <sfondeur@academic.rrc.ca>
2018-11-15 19:56:29 -0600

    Added provinces column to Users to prep for the Province model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
   # devise for users
-  devise_for :users
+  # devise_for :users
+
+  # our registration controller route
+  devise_for :users, :controllers => { registrations: 'registrations' }
   
   # active admin
   devise_for :admin_users, ActiveAdmin::Devise.config
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_16_010716) do
+ActiveRecord::Schema.define(version: 2018_11_16_015010) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 2018_11_16_010716) do
     t.string "address"
     t.string "lastname"
     t.string "postal_code"
+    t.string "province"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
