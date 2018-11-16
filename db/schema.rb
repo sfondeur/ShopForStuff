77c96495cf4be6873399300f56139e7073d21ddc
Shane <sfondeur@academic.rrc.ca>
2018-11-15 19:07:40 -0600

    Added new users and columns for the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_235949) do
+ActiveRecord::Schema.define(version: 2018_11_16_010716) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -108,6 +108,10 @@ ActiveRecord::Schema.define(version: 2018_11_15_235949) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "address"
+    t.string "lastname"
+    t.string "postal_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
